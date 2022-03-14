a671f61c2d935dcb596a759c8e04957d9a3de3db
erwin <winzon353535@gmail.com>
2022-03-14 15:52:57 -0700

    added movie restful api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,51 @@
 class MoviesController < ApplicationController
 
-  def all_movies
+  # def all_movies
+  #   movies = Movie.all
+  #   render json: movies.as_json
+  # end
+
+  # def first_movie
+  #   movie = Movie.find_by(id: 1)
+  #   render json: movie.as_json
+  # end
+
+  def index
     movies = Movie.all
     render json: movies.as_json
   end
 
-  def first_movie
-    movie = Movie.find_by(id: 1)
+  def show
+    movie_id = params[:id]
+    movie = Movie.find_by(id: movie_id)
+    render json: movie .as_json
+  end
+
+  def create
+    movie = Movie.new(
+      title: params["title"],
+      year: params["year"],
+      plot: params["plot"]
+    )
+    movie.save
     render json: movie.as_json
+  end
+
+  def update
+    movie_id = params[:id]
+    movie = Movie.find_by(id: movie_id)
+    movie.title = params["title"] || movie.title
+    movie.year = params["year"] || movie.year
+    movie.plot = params["plot"] || movie.plot
+    movie.save
+    render json: movie.as_json
+  end
+
+  def destroy
+    movie_id = params[:id]
+    movie = Movie.find_by(id: movie_id)
+    movie.destroy
+    render json: {message: "Movie deleted"}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,14 @@ Rails.application.routes.draw do
 
   delete "/actors/:id" => "actors#destroy"
 
+  get "/movies" => "movies#index"
+
+  get "movies/:id" => "movies#show"
+
+  post "/movies" => "movies#create"
+
+  patch "/movies/:id" => "movies#update"
+
+  delete "movies/:id" => "movies#destroy"
+
 end
