4dd03dbcac845762aa365bcc9cb5342f90686453
erwin <winzon353535@gmail.com>
2022-03-17 09:26:48 -0700

    add validates to actor

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,7 @@
 class Actor < ApplicationRecord
+  validates :first_name, length: { greater_than: 2 }
+  validates :last_name, length: { greather_than: 2 }
+  validates :known_for, presence: true
+  validates :age, numericality: { greater_than: 13 }
+
 end
