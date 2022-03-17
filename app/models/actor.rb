ac7c640c48de28b885416e87c121612328a8d2f8
erwin <winzon353535@gmail.com>
2022-03-17 14:13:10 -0700

    added validations to actor model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ApplicationRecord
-  validates :first_name, length: { greater_than: 2 }
-  validates :last_name, length: { greather_than: 2 }
+  validates :first_name, length: { minimum: 2 }
+  validates :last_name, length: { minimum: 2 }
   validates :known_for, presence: true
   validates :age, numericality: { greater_than: 13 }
 
