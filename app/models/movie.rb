3e32d3085d0eb7b3092e01cb59d0551bd93cfc8f
erwin <winzon353535@gmail.com>
2022-03-17 14:18:40 -0700

    added validations to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :plot, presence: true
+  validates :plot, length: { minimum: 5 }
+  validates :year, presence: true
+
 end
