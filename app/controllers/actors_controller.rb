23dbceed875b326ac125affc872137e938353d85
erwin <winzon353535@gmail.com>
2022-03-20 13:17:26 -0700

    fixed index show create update to support jb

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -38,24 +38,24 @@ class ActorsController < ApplicationController
       age: params["age"],
       movie_id: params["movie_id"]
     )
+    @actor = actor
     if actor.save
-      render json: actor.as_json
+      render template: "actors/show"
     else
       render json: {errors: actor.errors.full_messages}, status: 422
     end
   end
 
   def update
-    actor_id = params[:id]
-    actor = Actor.find_by(id: actor_id)
-    actor.first_name = params["first_name"] || actor.first_name
-    actor.last_name = params["last_name"] || actor.last_name
-    actor.known_for = params["known_for"] || actor.known_for
-    actor.gender = params["gender"] || actor.gender
-    actor.age = params["age"] || actor.age
-    actor.movie_id = params["movie_id"] || actor.movie_id
-    if actor.save
-      render json: actor.as_json
+    @actor = Actor.find_by(id: params[:id])
+    @actor.first_name = params["first_name"] || @actor.first_name
+    @actor.last_name = params["last_name"] || @actor.last_name
+    @actor.known_for = params["known_for"] || @actor.known_for
+    @actor.gender = params["gender"] || @actor.gender
+    @actor.age = params["age"] || @actor.age
+    @actor.movie_id = params["movie_id"] || @actor.movie_id
+    if @actor.save
+      render template: "actors/show"
     else
       render json: {errors: actor.errors.full_messages}, status: 422
     end
